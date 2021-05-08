cb06c60264568edf64282d6d16e55d6db810ac3a
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-05-08 15:37:10 +0200

    Add files via upload

---
+++ Osoba.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypyGeneryczneZadanie1
+{
+    class Osoba<T, T1, T2>
+    {
+
+        private T imie;
+        private T1 nazwisko;
+        private T2 date;
+
+        public Osoba(T imie, T1 nazwisko, T2 date)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.date = date;
+        }
+
+        public T Imie { get => imie; set => imie = value; }
+        public T1 Nazwisko { get => nazwisko; set => nazwisko = value; }
+        public T2 Date { get => date; set => date = value; }
+
+        public override string ToString()
+        {
+            return $"Osoba: {Imie}, {Nazwisko}, {Date}";
+        }
+
+        public static int Porownaj(Osoba<T, T1,T2> o1, Osoba<T, T1, T2> o2)
+        {
+            if (o1 == null && o2 == null) return 0;
+            else if (o1 == null) return -1;
+            else if (o2 == null) return 1;
+
+            if ((Convert.ToDateTime(o1.date))>(Convert.ToDateTime(o2.date))) return -1;
+            else if ((Convert.ToDateTime(o1.date)) < (Convert.ToDateTime(o2.date))) return 1;
+            else return 0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TypyGeneryczneZadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Osoba<string, string, string>[] tab ={
+
+                new Osoba<string, string, string>("Adam", "Krawczyk", "21/09/1978"),
+                new Osoba<string, string, string>("Ola", "Krawczyk", "09/10/1988"),
+                new Osoba<string, string, string>("Adam", "Kowalski", "10/08/1978"),
+                new Osoba<string, string, string>("Alicja", "Trad", "11/06/1988"),
+                new Osoba<string, string, string>("Stanislaw", "Gryf", "11/06/1978"),
+
+            };
+
+            Console.WriteLine("PRZED SORTOWANIEM");
+            foreach (var i in tab)
+            {
+               Console.WriteLine(i.ToString());
+            }
+
+            Array.Sort(tab, Osoba<string, string, string>.Porownaj);
+
+            Console.WriteLine("PO SORTOWANIU");
+            foreach (var i in tab)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+        }
+    }
+}
